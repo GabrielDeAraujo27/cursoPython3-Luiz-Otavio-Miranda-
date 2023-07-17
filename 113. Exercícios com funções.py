0d3d99145433d4d09d96b08e3b61e7cf4229be11
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-07-17 16:40:41 -0300

    Updated 113. Exercícios com funções.py

--- 113. Exercícios com funções.py
+++ 113. Exercícios com funções.py
@@ -1,8 +1,23 @@
-def multiplicacao(a, *b):
-    for c in b:
-        d += f'{a} *'
-        a = a * c
-    return d
+import random
 
-numMultiplicado = multiplicacao(1, 2, 8, 5, 4, 6, 7, 2, 9)
-print(f' = {numMultiplicado}')
+
+def multiplicacao(a, *b):
+    print(f'{a}', end=' ')
+    for c in b:
+        print(f'* {c}', end=' ')
+        a = a * c
+    return a
+
+def parOuImpar(*a):
+    for c in a:
+        if c % 2 == 0:
+            print(f'{c} é par')
+        else:
+            print(f'{c} é ímpar')
+
+escolha = str(input('Você deseja multiplicar números(1) ou saber se são pares ou impares(2)? '))
+if escolha in '1':
+    numMultiplicado = multiplicacao(random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9))
+    print(f' = {numMultiplicado}')
+elif escolha in '2':
+    parOuImpar(random.randint(0,80), random.randint(0,80), random.randint(0,80), random.randint(0,80), random.randint(0,80))
