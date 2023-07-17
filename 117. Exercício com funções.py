22ee298aebb36634242e6fa9ce3df03297d10408
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-07-17 17:32:31 -0300

    Update 117. Exercício com funções.py

--- 117. Exercício com funções.py
+++ 117. Exercício com funções.py
@@ -1,6 +1,20 @@
-def dupTripQuad(a)
+def dupTripQuad(a):
+    print(a)
     print(duplica(a))
     print(triplica(a))
     print(quadruplica(a))
 
-def 
+def duplica(a):
+    a = a * 2
+    return a
+
+def triplica(a):
+    a = a * 3
+    return a
+
+def quadruplica(a):
+    a = a * 4
+    return a
+
+x = float(input('Digite um número: '))
+dupTripQuad(x)
