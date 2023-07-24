0fc9b72834b71e139903c9af8e058a07a8f0ba00
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-07-24 18:36:34 -0300

    Update 169. Exercício - Unir listas.py

--- 169. Exercício - Unir listas.py
+++ 169. Exercício - Unir listas.py
@@ -1,23 +1,7 @@
-def analisaLista(cidades, estados):
-    if len(cidades)==len(estados):
-        uneCidadeEstado(cidades, estados)
-    elif len(cidades)>len(estados):
-        cidadesAdaptado = []
-        for c in range(0, len(estados)):
-            cidadesAdaptado.append(cidades[c])
-        uneCidadeEstado(cidadesAdaptado, estados)
-    else:
-        estadosAdaptado = []
-        for c in range(0, len(cidades)):
-            estadosAdaptado.append(estados[c])
-        uneCidadeEstado(cidades, estadosAdaptado)
-
 def uneCidadeEstado(x, y):
-    for c in range(0, len(y)):
+    for c in range(0, min(len(x), len(y))):
         print(x[c], y[c], sep=' - ')
 
 listaDeCidades = ['Salvador', 'Ubatuba', 'Belo Horizonte', 'Rio de Janeiro', 'Goiania']
 listaDeEstados = ['BA', 'SP', 'MG', 'RJ']
-analisaLista(listaDeCidades, listaDeEstados)
-
-'Futuramente será alterado...'
+uneCidadeEstado(listaDeCidades, listaDeEstados)
