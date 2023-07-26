54b1d5b49215b1a1ad83b44e6a874bf3040bc14a
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-07-26 00:05:19 -0300

    Update 171. Exercícios - somando duas listas.py

--- 171. Exercícios - somando duas listas.py
+++ 171. Exercícios - somando duas listas.py
@@ -1,7 +1,18 @@
 lista_a = [1, 2, 3, 4, 5, 6, 7]
 lista_b = [1, 2, 3, 4]
+
+#############################################
+
 #for c in range(0, min(len(lista_a), len(lista_b))):
 #    print(lista_a[c]+lista_b[c])
-listaSoma = [x + y for x,y in zip(lista_a, lista_b)]
-for c in listaSoma:
-    print(c, end=' ')
+listaSoma = iter([x + y for x,y in zip(lista_a, lista_b)])
+
+#############################################
+
+#for c in listaSoma:
+#    print(c, end=' ')
+while True:
+    try:
+        print(next(listaSoma))
+    except StopIteration:
+        break
