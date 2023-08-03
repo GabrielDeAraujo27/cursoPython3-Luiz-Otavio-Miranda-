39690c61d6002d2de39f48eb023b81bdc31fb108
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-08-03 17:22:05 -0300

    Create 288 a 290 exercicios com a lib JSON

---
+++ 288 a 290 exercicios com a lib JSON/__init__.py
@@ -0,0 +1,18 @@
+import json
+
+def e288():
+    with open("string.json", "r", encoding="UTF-8") as arquivo:
+        dadosAnteriores = json.load(arquivo)
+
+    string_json = 'string' + dadosAnteriores
+
+    #for c in dadosAnteriores:
+     #   string_json += c
+
+
+    with open("string.json", "w", encoding="UTF-8") as arquivo:
+        json.dump(string_json, arquivo, indent=4)
+
+
+
+e288()
