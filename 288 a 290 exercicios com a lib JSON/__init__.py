f82521ec8edc8b6da0c24bcfd69e9d31b668e1a6
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-08-03 17:26:15 -0300

    Update 288 a 290 exercicios com a lib JSON

--- 288 a 290 exercicios com a lib JSON/__init__.py
+++ 288 a 290 exercicios com a lib JSON/__init__.py
@@ -4,7 +4,7 @@ def e288():
     with open("string.json", "r", encoding="UTF-8") as arquivo:
         dadosAnteriores = json.load(arquivo)
 
-    string_json = 'string' + dadosAnteriores
+    string_json = 'string\n' + dadosAnteriores
 
     #for c in dadosAnteriores:
      #   string_json += c
@@ -12,6 +12,8 @@ def e288():
 
     with open("string.json", "w", encoding="UTF-8") as arquivo:
         json.dump(string_json, arquivo, indent=4)
+
+    print(string_json)
 
 
 
