62960825d1757d245298c72393a640d5e0e6f08f
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-08-08 20:20:34 -0300

    Update 206. Exercício - Salve sua classe em JSON

--- 206 Exercício - Salve sua classe em JSON/C.py
+++ 206 Exercício - Salve sua classe em JSON/C.py
@@ -1,9 +1,6 @@
 class pessoa:
-    def __init__(self, nome='', sobrenome='', idade=0, sexo='', indice=0, dicio=0):
+    def __init__(self, nome='', sobrenome='', idade=0, sexo=''):
         self.nome = nome
         self.sobrenome = sobrenome
         self.idade = idade
         self.sexo = sexo
-        #if __name__ != '__main__':
-         #   self
-
--- 206 Exercício - Salve sua classe em JSON/Cadastro.py
+++ 206 Exercício - Salve sua classe em JSON/Cadastro.py
@@ -1,8 +1,3 @@
-# Exercício - Salve sua classe em JSON
-# Salve os dados da sua classe em JSON
-# e depois crie novamente as instâncias
-# da classe com os dados salvos
-# Faça em arquivos separados.
 import json
 from C import pessoa
 
