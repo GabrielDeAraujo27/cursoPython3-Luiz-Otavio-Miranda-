a75ce34c8a905209c7d4e19901d4bcd266728c1a
Gabriel de Araújo <gabrieldearaujo321@gmail.com>
2023-08-11 00:07:04 -0300

    Update 218. Exercício com classes e relações.py

--- 218. Exercício com classes e relações.py
+++ 218. Exercício com classes e relações.py
@@ -1,40 +1,37 @@
 class marca:
-    def __init__(self, nomeMarca, nacionalidade):
+    def __init__(self, nomeMarca=''):
         self.nomeMarca = nomeMarca
-        self.nacionalidade = nacionalidade
 
     def defineMarca(self):
         self.nomeMarca = 'Honda' #input('Qual o nome da marca? ')
-        self.nacionalidade = 'Americano' #input('Qual a nacionalidade desse carro? ')
         return self
 
 class motor:
-    def __init__(self, nomeMotor='', potencia=''):
-        self.nomeMotor = nomeMotor
-        self.potencia = potencia
-
-    def defineMotor(self):
-        self.nomeMotor = '900b' #input('Qual o nome desse motor? ')
-        self.potencia = '70 cavalos' #input('Qual a potencia desse motor? ')
-        return self
+    def __init__(self, nomeMoto=''):
+        self.nomeMoto = nomeMoto
 
 class carro:
-    def __init__(self, nome='', marcab='', motorb=''):
-        self.nome = nome
+    def __init__(self, modelo='', marcab='', motorb=''):
+        self.modelo = modelo
         self.marcab = marcab
         self.motorb = motorb
-    def defineCarro(self):
-        self.nome = 'Sedan' #input('Qual a nome desse carro? ')
-        self.motorb = motor.defineMotor(self)
-        self.marcab = marca.defineMarca(self)
+
+    def defineCarro(self, modelo='', marcab='', motorb=''):
+        self.modelo = modelo #input('Qual a nome desse carro? ')
+        self.motorb = motorb
+        self.marcab = marcab
         return self
-meuCarro = carro()
-meuCarro = carro.defineCarro(meuCarro)
 
-print(meuCarro.nome)
+fiat = marca('Fiat')
+motor900b = motor('Motor 900b')
+volkswagen = marca('Volkswagen')
 
-print(meuCarro.marcab.nomeMarca)
-print(meuCarro.marcab.nacionalidade)
+meuCarro = carro
+meuCarro.defineCarro(meuCarro, 'Sedan', fiat, motor900b)
 
-print(meuCarro.motorb.nomeMotor)
-print(meuCarro.motorb.potencia)
+seuCarro = carro('Sport')
+seuCarro.motorb = motor900b
+seuCarro.marcab = volkswagen
+
+print(meuCarro.modelo,meuCarro.marcab.nomeMarca, meuCarro.motorb.nomeMoto)
+print(seuCarro.modelo,seuCarro.marcab.nomeMarca, seuCarro.motorb.nomeMoto)
